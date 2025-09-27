bb3a1519a33334adfba2202587a6c4436741ae00
JOHNpCADAROO <126931681+JohnPerez012@users.noreply.github.com>
2025-09-27 18:20:17 +0800

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, serverTimestamp, deleteDoc, doc, getDocs, updateDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 import { getAuth, signInAnonymously, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+console.log("Active tab:", document.querySelector(".tab.active"));
+console.log("Floating container:", floatingTabContainer);
 
 // Firebase Config - Consider moving to environment variables in production
 const firebaseConfig = {
